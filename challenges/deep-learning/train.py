0978b2001e3b3652105da25b82fa07d63c72837c
dev-onejun <wjjgjk@naver.com>
2024-05-14 14:51:49 +0900

    UPDATE: autocheck `saved_models` dir && FIX: typo

--- challenges/deep-learning/train.py
+++ challenges/deep-learning/train.py
@@ -9,15 +9,15 @@ from transformers import ViTImageProcessor
 from torch import nn
 from models import ViTClassifier
 
-# from datasets import RecaptchaDataset as Dataset
-from datasets import Cifar10 as Dataset
+from datasets import RecaptchaDataset as Dataset
+# from datasets import Cifar10 as Dataset
 from utils.train_helpers import fit, evaluate
 
 from torch.utils.tensorboard.writer import SummaryWriter
 
 
 def train(train_loader, validate_loader, processor, model, n_epochs, lr):
-    # optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     """
     optimizer = torch.optim.Adam(
         filter(lambda p: p.requires_grad, model.parameters()), lr=lr
@@ -44,25 +44,25 @@ def train(train_loader, validate_loader, processor, model, n_epochs, lr):
         )
 
         print(
-            f"Epoch {epoch}\t|\tTrain Loss {train_loss:.4f}\tTrain Accuracy {train_accuracy:.4f}\tTest Loss {validate_loss:.4f}\tTest Accuracy {validate_accuracy:.4f}"
+            f"Epoch {epoch}\t|\tTrain Loss {train_loss:.4f}\tTrain Accuracy {train_accuracy:.4f}\tValidate Loss {validate_loss:.4f}\tValidate Accuracy {validate_accuracy:.4f}"
         )
-
-        if epoch % 10 == 0:
-            torch.save(
-                model.state_dict(),
-                os.path.join(".", "saved_models", "epoch_{}.pt".format(epoch)),
-            )
 
         writer.add_scalar("loss/train", train_loss, epoch)
         writer.add_scalar("accuracy/train", train_accuracy, epoch)
         writer.add_scalar("loss/validate", validate_loss, epoch)
         writer.add_scalar("accuracy/validate", validate_accuracy, epoch)
 
+        if epoch % 10 == 0:
+            torch.save(
+                model.state_dict(),
+                os.path.join(saved_model_dir, "epoch_{}.pt".format(epoch)),
+            )
+
     writer.flush()
 
     torch.save(
         model.state_dict(),
-        os.path.join(".", "saved_models", "final_{}.pt".format(datetime.now())),
+        os.path.join(saved_model_dir, "final_{}.pt".format(datetime.now())),
     )
 
 
@@ -83,10 +83,8 @@ def main():
         model = nn.DataParallel(model)
 
     if args.train:
-        # train_dataset = RecaptchaDataset(args.dataset_path, "train")
-        # validate_dataset = RecaptchaDataset(args.dataset_path, "validate")
-        train_dataset = Dataset("./datasets/cifar10-dataset", "train")
-        validate_dataset = Dataset("./datasets/cifar10-dataset", "validate")
+        train_dataset = Dataset(args.dataset_path, "train")
+        validate_dataset = Dataset(args.dataset_path, "validate")
 
         train_loader = data.DataLoader(
             train_dataset,
@@ -115,7 +113,7 @@ def main():
         ), "A model file to test should be given with the --model-path argument"
         model.load_state_dict(torch.load(args.model_path))
 
-        test_dataset = RecaptchaDataset(args.dataset_path, "test")
+        test_dataset = Dataset(args.dataset_path, "test")
         test_loader = data.DataLoader(
             test_dataset,
             batch_size=args.batch_size,
@@ -139,6 +137,9 @@ if args.cuda:
 device = torch.device("cuda" if args.cuda else "cpu")
 
 torch.manual_seed(args.seed)
+
+saved_model_dir = "./saved_models"
+os.makedirs(saved_model_dir, exist_ok=True)
 
 if __name__ == "__main__":
     """A ViT Training Script for Computer Vision Challenges"""
